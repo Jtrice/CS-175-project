da032ca8209e47c3a68e14db27d7fb0796a33d34
Jtrice <wildhare82@gmail.com>
2015-03-13 12:34:04 -0700

    Added documentation, moved code to function, added weights
    Moved all the main code from main to a separate function so it can be
    run from outside. Added documentation to make functionality clearer.
    Added weights to accuracy checking, can be adjusted simply. Still needs
    output function.

--- s1/bernoulliNBclassifier.py
+++ s1/bernoulliNBclassifier.py
@@ -10,6 +10,8 @@ from sklearn.naive_bayes import BernoulliNB
 import splitTestTrainingData
 
 
+#Takes the training data as a list of lists, the classes for each list, and the values to be predicted,
+#   and returns the predictions.
 def bernNBClassifier(trainingVectors, targetValues, valueForPrediction):
     
     clf = BernoulliNB()
@@ -17,6 +19,36 @@ def bernNBClassifier(trainingVectors, targetValues, valueForPrediction):
     
     #taken from the example, likely needs to change with predictions. 
     return(clf.predict(valueForPrediction))
+
+
+#Takes a file path as a string, runs Bernoulli Naive Bayes classifier on the data.
+def runBernoulliNBClassifier(filename):
+    trainingData, testData, frontPage, testFrontPage = splitTestTrainingData.formatForBernoulli(fileName, .25)
+    predictions = bernNBClassifier(trainingData, frontPage, testData)
+    
+    #values to track the accumulated 'total' points and the 'correct' points, represented
+    #    represented by the given weights
+    correct = 0
+    total = 0
+    
+    #weights for correctly/incorrectly guessing a post will/won't make it to the front page
+    frontPageWeight = 100
+    notFrontPageWeight = 1
+    
+    #calculates accuracy with given weights
+    for i in range(len(predictions)):
+        if(testFrontPage[i] == '1'):
+            total += frontPageWeight
+        else:
+            total += notFrontPageWeight
+        if((predictions[i] == testFrontPage[i]) and (predictions[i] == '1')):
+            correct += frontPageWeight
+        elif((predictions[i] == testFrontPage[i]) and (predictions[i] == '0')):
+            correct += notFrontPageWeight
+            
+    #returns the accuracy as a percentage        
+    return(correct/total)
+    
     
 if __name__ == "__main__":
     
@@ -24,20 +56,16 @@ if __name__ == "__main__":
     #x = np.random.randint(2, size=(6,100))
     #y = np.array([1,2,3,4,4,5])
     
-    fileName = "C:\\Users\\Jeremy\\Documents\\CS 175\\sample - Copy.csv"
-    trainingData, testData, frontPage, testFrontPage = splitTestTrainingData.formatForBernoulli(fileName, .25)
-
+    fileName = "C:\\Users\\Jeremy\\Documents\\CS 175\\NEWdataBETTERdataUSEthis.csv"
+    print(runBernoulliNBClassifier(fileName))
 
     #gets the predictions from the classifier    
     #print(bernNBClassifier(x, y, x[2]))
-    predictions = bernNBClassifier(trainingData, frontPage, testData)
+    
     
     #tracks how many correct answers the predictions get, needs weights added
-    correct = 0
-    for i in range(len(predictions)):
-        if(predictions[i] == testFrontPage[i]):
-            correct += 1
-    print(correct/(len(predictions)))
+    
+
     
     
     #print(trainingData)
